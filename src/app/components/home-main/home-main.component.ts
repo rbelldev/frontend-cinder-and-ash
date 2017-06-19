c1686ade1532b4df1ac4362d5dc4c8b3a4eb4c16
Rob <rob.bell.86@gmail.com>
2017-06-19 14:59:53 -0400

    Updated Home Page with ToS Raid Videos and Guides

--- src/app/components/home-main/home-main.component.ts
+++ src/app/components/home-main/home-main.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import {Component} from "@angular/core";
 @Component({
   selector: 'home-main',
   templateUrl: 'home-main.component.html',
@@ -8,22 +7,70 @@ import { Component } from '@angular/core';
 export class HomeMainComponent {
 
   youtubeEmbedBassUrl: string = "https://www.youtube.com/embed/";
+  wowheadLinkBaseUrl: string = "http://www.wowhead.com/";
 
   raidVideos = [
-    {id:"skorpyron", bossName:"Skorpyron", youtubeUrl: `${this.youtubeEmbedBassUrl}ZfGTCLNTxIw`},
-    {id:"chronomatic-anomaly",bossName:"Chronomatic Anomaly", youtubeUrl: `${this.youtubeEmbedBassUrl}xCRT1OmH3Io`},
-    {id:"trilliax",bossName:"Trilliax", youtubeUrl: `${this.youtubeEmbedBassUrl}xbUDaWIj2Zw`},
-    {id:"spellblade-aluriel",bossName:"Spellblade Aluriel", youtubeUrl: `${this.youtubeEmbedBassUrl}IEy1JBsh5-Y`},
-    {id:"krosus",bossName:"Krosus", youtubeUrl: `${this.youtubeEmbedBassUrl}wbKBcQCpNio`},
-    {id:"high-botanist-telarn",bossName:"High Botanist Tel'arn", youtubeUrl: `${this.youtubeEmbedBassUrl}UEwFsHxaNzM`},
-    {id:"tichondrius",bossName:"Tichondrius", youtubeUrl: `${this.youtubeEmbedBassUrl}72H_dmJFi0g`},
+    {
+      id: "goroth",
+      bossName: "Goroth",
+      youtubeUrl: `${this.youtubeEmbedBassUrl}w04v2V8V7Y8`,
+      wowheadLink: `${this.wowheadLinkBaseUrl}goroth-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
+    {
+      id: "demonic-inquisition",
+      bossName: "Demonic Inquisition",
+      youtubeUrl: `${this.youtubeEmbedBassUrl}7GB071_cQlA`,
+      wowheadLink: `${this.wowheadLinkBaseUrl}demonic-inquisition-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
+    {
+      id: "harjatan",
+      bossName: "Harjatan",
+      youtubeUrl: `${this.youtubeEmbedBassUrl}unJeZoac0mw`,
+      wowheadLink: `${this.wowheadLinkBaseUrl}harjatan-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
+    {
+      id: "mistress-sasszine",
+      bossName: "Mistress Sassz'ine",
+      wowheadLink: `${this.wowheadLinkBaseUrl}mistress-sasszine-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
+    {
+      id: "sisters-of-the-moon",
+      bossName: "Sisters of the Moon",
+      wowheadLink: `${this.wowheadLinkBaseUrl}sisters-of-the-moon-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
+    {
+      id: "maiden-of-vigilance",
+      bossName: "Maiden of Vigilance",
+      youtubeUrl: `${this.youtubeEmbedBassUrl}f2va3KPFZHs`,
+      wowheadLink: `${this.wowheadLinkBaseUrl}maiden-of-vigilance-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
+    {
+      id: "fallen-avatar",
+      bossName: "Fallen Avatar",
+      youtubeUrl: `${this.youtubeEmbedBassUrl}_h9jW41xeHw`,
+      wowheadLink: `${this.wowheadLinkBaseUrl}fallen-avatar-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
+    {
+      id: "kiljaeden",
+      bossName: "Kil'Jaeden",
+      youtubeUrl: `${this.youtubeEmbedBassUrl}GKL-wDCKwCk`,
+      wowheadLink: `${this.wowheadLinkBaseUrl}kiljaeden-tomb-of-sargeras-raid-strategy-guide`,
+      ptr:true
+    },
   ]
 
-  navigateToRaidVideo(id:string){
+  navigateToRaidVideo(id: string) {
     window.location.hash = `#${id}`
   }
 
-  navigateToTop(){
+  navigateToTop() {
     window.location.hash = `#top`
   }
 
